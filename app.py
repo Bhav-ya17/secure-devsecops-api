4dea0defb1546808ff91fcc211e76bef52fb1cd9
Bhavya Dudhat <bdudhat1705@gmail.com>
2026-09-19 11:12:06 -0400

    Documenting Intentional Docker Host Binding

--- app.py
+++ app.py
@@ -24,8 +24,6 @@ def status():
         "environment": "production"
     }
 
-
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",  # nosemgrep: python.flask.security.audit.app-run-param-config.avoid_app_run_with_bad_host
-        port=5000)
+    # nosemgrep: python.flask.security.audit.app-run-param-config.avoid_app_run_with_bad_host
+    app.run(host="0.0.0.0", port=5000)
